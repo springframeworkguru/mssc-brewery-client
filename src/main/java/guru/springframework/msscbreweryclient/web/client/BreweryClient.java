11f6472e8948cbfd25149f54be376a86762ef60d
John Thompson <john@springframework.guru>
2019-04-23 22:13:33 -0400

    adding get client

---
+++ src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
@@ -0,0 +1,34 @@
+package guru.springframework.msscbreweryclient.web.client;
+
+import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+/**
+ * Created by jt on 2019-04-23.
+ */
+@ConfigurationProperties(prefix = "sfg.brewery", ignoreUnknownFields = false)
+@Component
+public class BreweryClient {
+
+    public final String BEER_PATH_V1 = "/api/v1/beer/";
+    private String apihost;
+
+    private final RestTemplate restTemplate;
+
+    public BreweryClient(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public BeerDto getBeerById(UUID uuid){
+        return restTemplate.getForObject(apihost + BEER_PATH_V1 + uuid.toString(), BeerDto.class);
+    }
+
+    public void setApihost(String apihost) {
+        this.apihost = apihost;
+    }
+}
---
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,25 @@
+package guru.springframework.msscbreweryclient.web.client;
+
+import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient client;
+
+    @Test
+    void getBeerById() {
+        BeerDto dto = client.getBeerById(UUID.randomUUID());
+
+        assertNotNull(dto);
+
+    }
+}
