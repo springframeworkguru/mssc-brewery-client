3c048e1338fa9e26fc5fecaaf5c55d6574146af1
Harrison Brock <harrison@harrisonbrock.com>
2020-04-05 09:24:01 -0400

    Removed extra forward slash on updated and delete

--- src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
+++ src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
@@ -34,11 +34,11 @@ public class BreweryClient {
     }
 
     public void updateBeer(UUID uuid, BeerDto beerDto){
-        restTemplate.put(apihost + BEER_PATH_V1 + "/" + uuid.toString(), beerDto);
+        restTemplate.put(apihost + BEER_PATH_V1 + uuid.toString(), beerDto);
     }
 
     public void deleteBeer(UUID uuid){
-        restTemplate.delete(apihost + BEER_PATH_V1 + "/" + uuid );
+        restTemplate.delete(apihost + BEER_PATH_V1 + uuid );
     }
 
     public void setApihost(String apihost) {
