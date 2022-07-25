14d5bf822f3c5752f23c9ddf1ea8d7c72431db9f
Mauro Castillo RondinaraÃ <mauro.castillo@globant.com>
2022-07-25 13:04:34 -0300

    externalize variables

---
+++ src/main/java/guru/springframework/msscbreweryclient/web/config/BlockingRestTemplateCustomizer.java
@@ -0,0 +1,59 @@
+package guru.springframework.msscbreweryclient.web.config;
+
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
+
+  private final Integer maxTotalConnections;
+  private final Integer defaultMaxTotalConnections;
+  private final Integer connectionRequestTimeout;
+  private final Integer socketTimeout;
+
+  public BlockingRestTemplateCustomizer(@Value("sfg.maxtotalconnections") Integer maxTotalConnections,
+                                        @Value("sfg.defaultmaxtotalconnections") Integer defaultMaxTotalConnections,
+                                        @Value("sfg.connectionsrequesttimeout") Integer connectionRequestTimeout,
+                                        @Value("sfg.sockettimeout") Integer socketTimeout) {
+    this.maxTotalConnections = maxTotalConnections;
+    this.defaultMaxTotalConnections = defaultMaxTotalConnections;
+    this.connectionRequestTimeout = connectionRequestTimeout;
+    this.socketTimeout = socketTimeout;
+  }
+
+
+  public ClientHttpRequestFactory clientHttpRequestFactory() {
+    PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+    connectionManager.setMaxTotal(maxTotalConnections);
+    connectionManager.setDefaultMaxPerRoute(defaultMaxTotalConnections);
+
+    RequestConfig requestConfig = RequestConfig
+        .custom()
+        .setConnectionRequestTimeout(connectionRequestTimeout)
+        .setSocketTimeout(socketTimeout).build();
+
+    CloseableHttpClient httpClient = HttpClients
+        .custom()
+        .setConnectionManager(connectionManager)
+        .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())
+        .setDefaultRequestConfig(requestConfig)
+        .build();
+
+    return new HttpComponentsClientHttpRequestFactory(httpClient);
+  }
+
+
+  @Override
+  public void customize(RestTemplate restTemplate) {
+    restTemplate.setRequestFactory(this.clientHttpRequestFactory());
+  }
+}
---
+++ src/main/java/guru/springframework/msscbreweryclient/web/config/NIORestTemplateCustomizer.java
@@ -0,0 +1,44 @@
+package guru.springframework.msscbreweryclient.web.config;
+
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
+import org.apache.http.impl.nio.conn.PoolingNHttpClientConnectionManager;
+import org.apache.http.impl.nio.reactor.DefaultConnectingIOReactor;
+import org.apache.http.impl.nio.reactor.IOReactorConfig;
+import org.apache.http.nio.reactor.IOReactorException;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsAsyncClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+//@Component
+public class NIORestTemplateCustomizer implements RestTemplateCustomizer {
+
+
+  public ClientHttpRequestFactory clientHttpRequestFactory() throws IOReactorException {
+    final DefaultConnectingIOReactor ioreactor = new DefaultConnectingIOReactor(IOReactorConfig.custom()
+        .setConnectTimeout(3000)
+        .setIoThreadCount(4)
+        .setSoTimeout(3000)
+        .build());
+
+    final PoolingNHttpClientConnectionManager connectionManager = new PoolingNHttpClientConnectionManager(ioreactor);
+    connectionManager.setDefaultMaxPerRoute(100);
+    connectionManager.setMaxTotal(1000);
+
+    CloseableHttpAsyncClient httpAsyncClient = HttpAsyncClients.custom()
+        .setConnectionManager(connectionManager)
+        .build();
+
+    return new HttpComponentsAsyncClientHttpRequestFactory(httpAsyncClient);
+  }
+
+  @Override
+  public void customize(RestTemplate restTemplate) {
+    try {
+      restTemplate.setRequestFactory(clientHttpRequestFactory());
+    } catch (IOReactorException e) {
+      e.printStackTrace();
+    }
+  }
+}
